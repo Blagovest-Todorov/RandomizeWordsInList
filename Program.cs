7d2ebcb05545cbbe424ee9e78c0325956d68818d
Blagovest Todorov <72020361+Blagovest-Todorov@users.noreply.github.com>
2021-02-19 08:37:45 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,18 +12,15 @@ namespace RandomizeWords
                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
                 .ToList(); // receive words -split them into a List of strings
 
-            // Hi, come, new  here
+            // Hi, come, new  here - ----> here come , new , Hi 
             string currWord = string.Empty;
-            Random newPosition = new Random(); //create an Object of type Random
-            
+            Random newPosition = new Random(); //create an Object of type Random            
 
             for (int i = 0; i < words.Count; i++)
             {
-                int position = newPosition.Next(0, words.Count);  // in Range 0 .. count-1
-
+                int position = newPosition.Next(0, words.Count);  // takes in Range 0 .. Count-1
                 currWord = words[i];  // We keep here the currWord
-
-                words[i] = words[position]; // We change the value od current Word 
+                words[i] = words[position]; // We change the value of current Word 
                 words[position] = currWord;
 
             }
